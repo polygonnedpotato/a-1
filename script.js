115dfb6416f5001a86533e31d687f4c462b98880
elburg the polygonned potato <berge61319@loswego.k12.or.us>
2021-02-24 00:03:21 +0000

    some edits and removals of unused files.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+console.log("script.js [/script.js] loaded successfully")
 document.cookie="Coder100=Who_are_you?"
+console.log("script.js [/script.js] finished")
 
 function listing() {
+  console.log("listing() [/script.js] activated")
   window.location.href = 'listing.html'
+  console.log("listing() [/script.js] finished")
 }
+console.log("script.js [/script.js] reached end of file")
